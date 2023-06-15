dbf29f7c50040a4ce689cd47eff5248f0afbb346
martyr00 <leonyvb04@gmail.com>
2023-06-15 03:00:54 +0300

    add rockets on screen

--- shooter.py
+++ shooter.py
@@ -10,25 +10,29 @@ pygame.display.set_caption("Shooter")
 fighter_img = pygame.image.load('img/fighter.png')
 fighter_wight, fighter_height = fighter_img.get_size()
 fighter_x, fighter_y = DISPLAY[0] / 2 - fighter_wight / 2, DISPLAY[1] - fighter_height
+fighter_is_moving_left, fighter_is_moving_right = False, False
+
 
 enemy_img = pygame.image.load('img/enemy.png')
 enemy_wight, enemy_height = enemy_img.get_size()
 
 rockets_img = pygame.image.load('img/rockets.png')
 rockets_wight, rockets_height = rockets_img.get_size()
+rockets_x, rockets_y = fighter_x + fighter_wight / 2 - rockets_wight / 2, fighter_y
+rockets_was_fired = False
 
-fighter_is_moving_left, fighter_is_moving_right = False, False
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_a or event.key == pygame.K_LEFT:
                 fighter_is_moving_left = True
             if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
                 fighter_is_moving_right = True
+            if event.key == pygame.K_SPACE:
+                rockets_was_fired = True
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_a or event.key == pygame.K_LEFT:
                 fighter_is_moving_left = False
@@ -42,5 +46,8 @@ while True:
 
     screen.blit(pygame.image.load('img/display.png'), (0, 0))
     screen.blit(fighter_img, (fighter_x, fighter_y))
+
+    if rockets_was_fired:
+        screen.blit(rockets_img, (rockets_x, rockets_y))
 
     pygame.display.update()
