1edac6ca35e0abbb1a9882019de8ff21b3517362
martyr00 <leonyvb04@gmail.com>
2023-06-15 03:12:53 +0300

    fun for rockets

--- shooter.py
+++ shooter.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from constants_for_shooter_game import *
+from random import randint
 
 pygame.init()
 
@@ -15,6 +16,7 @@ fighter_is_moving_left, fighter_is_moving_right = False, False
 
 enemy_img = pygame.image.load('img/enemy.png')
 enemy_wight, enemy_height = enemy_img.get_size()
+enemy_x, enemy_y = randint(0, DISPLAY[0]-enemy_wight), 0
 
 rockets_img = pygame.image.load('img/rockets.png')
 rockets_wight, rockets_height = rockets_img.get_size()
@@ -33,6 +35,7 @@ while True:
                 fighter_is_moving_right = True
             if event.key == pygame.K_SPACE:
                 rockets_was_fired = True
+                rockets_x, rockets_y = fighter_x + fighter_wight / 2 - rockets_wight / 2, fighter_y
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_a or event.key == pygame.K_LEFT:
                 fighter_is_moving_left = False
@@ -43,6 +46,11 @@ while True:
         fighter_x -= FIGHTER_STEP
     if fighter_is_moving_right and fighter_x <= DISPLAY[0] - fighter_wight - FIGHTER_STEP:
         fighter_x += FIGHTER_STEP
+
+    if rockets_was_fired:
+        rockets_y -= ROCKETS_STEP
+    if rockets_was_fired and rockets_y + rockets_height < 0:
+        rockets_was_fired = False
 
     screen.blit(pygame.image.load('img/display.png'), (0, 0))
     screen.blit(fighter_img, (fighter_x, fighter_y))
